7b2570839f966014393d31a978b876084094ce4b
Gonéri Le Bouder <goneri@lebouder.net>
2023-01-19 18:33:18 -0500

    initial import of the module and the tests

--- anonymizor/anonymizor.py
+++ anonymizor/anonymizor.py
@@ -1,1 +1,228 @@
-"""Main module."""
+#!/usr/bin/env python3
+
+import ciso8601
+import ipaddress
+import json
+import logging
+import random
+import re
+import time
+import yaml
+
+from detect_secrets.plugins import keyword
+from password_strength import PasswordStats
+
+
+logger = logging.getLogger(__name__)
+
+
+def gen_sample_password():
+    colours = ["red", "blue", "green", "yellow", "purple", "pink", "orange", "brown"]
+    fruits = ["papaya", "peach", "apple", "pineapple", "watermelon", "raspberry"]
+    countries = ["senegal", "japan", "ukraine"]
+
+    return f"{random.choice(colours)}-{random.choice(fruits)}-{random.choice(countries)}"
+
+
+def gen_email_address(*kwargs):
+    samples = [
+        "liam",
+        "olivia",
+        "noah",
+        "emma",
+        "oliver",
+        "charlotte",
+        "elijah",
+        "amelia",
+        "james",
+        "ava",
+        "william",
+        "sophia",
+        "benjamin",
+        "isabella",
+        "lucas",
+        "mia",
+        "henry",
+        "evelyn",
+        "theodore",
+        "harper",
+    ]
+    return f"{random.choice(samples)}{random.randint(0, 100)}@example.com"
+
+
+def is_jinja2(content: str) -> bool:
+    flags = re.MULTILINE | re.DOTALL
+    if bool(re.search(r"{{.*\w+.*}}", content, flags)):
+        return True
+    if bool(re.search(r"{%.*\w+.*%}", content, flags)):
+        return True
+    return False
+
+
+def is_ip_address(content: str):
+    try:
+        ip = ipaddress.ip_address(content)
+    except ValueError:
+        return False
+    return True
+
+
+def is_email_address(content: str) -> bool:
+    return bool(re.match(r".*\w+@[a-z\.]+[a-z]{2,}.*", content, flags=re.IGNORECASE))
+
+
+def is_date(content: str) -> bool:
+    # Py3.11 comes with a very performant solution
+    # https://github.com/closeio/ciso8601/blob/master/why_ciso8601.md#do-you-need-to-support-python--311
+    try:
+        return bool(ciso8601.parse_datetime(content))
+    except ValueError:
+        return False
+
+
+def is_password(content):
+    stats = PasswordStats(content)
+    return stats.strength() > 0.48
+
+
+
+
+def sensitive_field_name(name: str) -> bool:
+    return re.search(keyword.DENYLIST_REGEX_WITH_PREFIX, name) is not None
+
+
+def remove_email(value: str) -> str:
+    return re.sub(r".*(\w+@[a-z\.]+[a-z]{2,}).*", gen_email_address, value)
+
+
+
+common_ipv4_networks = [
+    ipaddress.IPv4Network("1.0.0.1/32"),
+    ipaddress.IPv4Network("1.1.1.1/32"),
+    ipaddress.IPv4Network("149.112.112.112/32"),
+    ipaddress.IPv4Network("208.67.220.220/32"),
+    ipaddress.IPv4Network("208.67.222.222/32"),
+    ipaddress.IPv4Network("76.223.122.150/32"),
+    ipaddress.IPv4Network("76.76.19.19/32"),
+    ipaddress.IPv4Network("8.20.247.20/32"),
+    ipaddress.IPv4Network("8.26.56.26/32"),
+    ipaddress.IPv4Network("8.8.4.4/32"),
+    ipaddress.IPv4Network("8.8.8.8/32"),
+    ipaddress.IPv4Network("9.9.9.9/32"),
+    ipaddress.IPv4Network("94.140.14.14/32"),
+    ipaddress.IPv4Network("94.140.15.15/32"),
+]
+
+
+def redact_ipv4_address(value: ipaddress.IPv4Address) -> ipaddress.IPv4Address:
+    for i in common_ipv4_networks:
+        if value in i:
+            return value.exploded
+    return value + random.randint(0, 100)
+
+
+common_ipv6_networks = [
+    ipaddress.IPv6Network("2001:4860:4860::8888/128"),
+    ipaddress.IPv6Network("2001:4860:4860::8844/128"),
+]
+
+
+def redact_ipv6_address(value: ipaddress.IPv6Address) -> ipaddress.IPv6Address:
+    for i in common_ipv6_networks:
+        if value in i:
+            return value.compressed
+
+    def randomize(block):
+        print(block.group(0))
+        field = block.group(0)[1:]
+        as_int = int(field, 16)
+        new_val = random.randint(0, as_int)
+        as_hex = hex(new_val)
+        hex_without_0x_prefix = as_hex[2:]
+        return ":" + hex_without_0x_prefix
+
+    print(f"-> {value.compressed}")
+    new_address = ipaddress.IPv6Address(
+        re.sub(r":[a-z0-9]+", randomize, value.compressed, flags=re.IGNORECASE)
+    )
+    return new_address
+
+
+
+def redact_ip_address(value: str) -> str:
+    func = {4: redact_ipv4_address, 6: redact_ipv6_address}
+    ip = ipaddress.ip_address(value)
+    return str(func[ip.version](ip))
+
+
+def anonymize_field(value: str, name) -> str:
+    value = value.strip()
+    if not value:
+        return value
+    elif is_jinja2(value):
+        return value
+    elif is_ip_address(value):
+        return redact_ip_address(value)
+    elif is_email_address(value):
+        return remove_email(value)
+
+    if sensitive_field_name(name):
+        if is_path(value):
+            return value
+        elif is_date(value):
+            return value
+        elif is_password(value):
+            return gen_sample_password()
+    return value
+
+
+def is_path(content: str):
+    # Rather conservative on purpose to avoid a false
+    # positive
+    return bool(re.match(r"^(|~)[a-z0-9_/\.-]+$", content, flags=re.IGNORECASE))
+
+
+def walker(o, key_name=''):
+    def key_name_str(k):
+        return k if isinstance(k, str) else ''
+
+    if key_name and not isinstance(key_name, str):
+        key_name = str(key_name)
+    if isinstance(o, dict):
+        return {k: walker(v, key_name=key_name_str(k)) for k, v in o.items()}
+    elif isinstance(o, list):
+        return [walker(v) for v in o]
+    elif isinstance(o, str):
+        return anonymize_field(o, key_name)
+    return o
+
+
+def anonymize(predictions):
+    def cleanup(p):
+        py_struct = yaml.safe_load(p)
+        clean = walker(py_struct)
+        return yaml.dump(clean)
+
+    return [cleanup(p) for p in predictions]
+
+
+
+def benchmark():
+    cpt = 0
+    duration = 0
+    with open("/home/goneri/Downloads/awft_v2.4.2_train.json", "r") as fd:
+        while True:
+            line = fd.readline()
+            if not line:
+                break
+            train_entry = json.loads(line)
+            from pprint import pprint
+
+            prediction = yaml.safe_load(train_entry["output_script"])
+            start = time.perf_counter()
+            ano = anonymize(prediction)
+            duration += time.perf_counter() - start
+            cpt += 1
+    print(f"cpt: {cpt}")
+    print(f"total duration: {duration}")
+    print(f"average: {duration/cpt}")
--- tests/test_anonymizor.py
+++ tests/test_anonymizor.py
@@ -1,24 +1,132 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-"""Tests for `anonymizor` package."""
+from ipaddress import IPv4Address, IPv4Network, IPv6Address, IPv6Network
+import yaml
 
-import pytest
+from anonymizor.anonymizor import (
+    is_jinja2,
+    is_path,
+    is_email_address,
+    is_date,
+    is_password,
+    sensitive_field_name,
+    redact_ipv4_address,
+    remove_email,
+    redact_ipv4_address,
+    redact_ipv6_address,
+    redact_ip_address,
+    anonymize_field,
+    walker,
+    anonymize,
+)
 
 
-from anonymizor import anonymizor
+def test_is_jinja2():
+    assert is_jinja2("  {{\n\nfoo\n   }}\n  \n") is True
+    assert is_jinja2("  {%\n\nfoo\n   %}\n  \n") is True
 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+def test_is_path():
+    assert is_path("/etc/fstab") is True
+    assert is_path("./opt/fstab") is True
+    assert is_path("~/.ssh/id_rsa.pub") is True
+    assert is_path(".%/mypassword/f$b") is False
+    assert is_path("certificates/CA.key") is True
 
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+def test_is_email_address():
+    assert is_email_address("contact@.somewhe.re") is True
+    assert is_email_address("contact@somewhe.re") is True
+    assert is_email_address("contact.somewhe.re") is False
+    assert is_email_address("été@somewhe.social") is True
+    assert is_email_address("some text with an email  a@somewhe.social  fff") is True
+
+
+def test_is_date():
+    assert is_date("2022-01-19 23:59:59") is True
+    assert is_date("Internal Users") is False
+
+
+def test_is_password() -> bool:
+    assert is_password("foobar") is False
+    assert is_password("1!!3@foobar%") is True
+
+
+def test_sensitive_field_name():
+    assert sensitive_field_name("login") is False
+    assert sensitive_field_name("password") is True
+    assert sensitive_field_name("passwd") is True
+    assert sensitive_field_name("db_passwd") is True
+    assert sensitive_field_name("db_passwd") is True
+
+
+def test_remove_email():
+    assert remove_email("fooo@bar.email").endswith("example.com")
+    assert remove_email("foo") == "foo"
+    assert "foo.bar@bar.re" not in remove_email("fo foo.bar@bar.re o")
+
+
+def test_redact_ipv4_address():
+    assert redact_ipv4_address(IPv4Address("192.168.3.5")) in IPv4Network(
+        "192.168.3.0/24"
+    )
+    assert redact_ipv4_address(IPv4Address("8.8.8.8")) == "8.8.8.8"
+    assert redact_ipv4_address(IPv4Address("8.8.8.9")) in IPv4Network("8.8.8.0/24")
+
+
+def test_redact_ipv6_address():
+    assert (
+        redact_ipv6_address(IPv6Address("2001:4860:4860::8888"))
+        == "2001:4860:4860::8888"
+    )
+    assert IPv6Address(
+        redact_ipv6_address(IPv6Address("2001:db8:3333:4444:5555:6666:7777:8888"))
+    )
+
+
+def test_refact_ip_address():
+    assert redact_ip_address("2001:4860:4860::8888") == "2001:4860:4860::8888"
+    assert IPv4Address(redact_ip_address("8.8.8.9"))
+
+
+def test_walker():
+    in_ = {
+        "name": "Install nginx and nodejs 12",
+        "apt": {"name": ["nginx", "nodejs"], "state": "latest"},
+    }
+    assert walker(in_) == in_
+
+    in_ = {
+        "name": "foo@montreal.ca",
+        "a_module": {
+            "ip": ["2001:460:48::888", "192.168.1.1"],
+            "password": "@This-should-disapear!",
+        },
+    }
+    changed = walker(in_)
+    assert "foo@montreal.ca" not in changed["name"]
+    assert "2001:460:48::888" not in changed["a_module"]["ip"]
+    assert "@This" not in changed["a_module"]["password"]
+
+
+def test_anonymize_no_change():
+    sample = [
+        "- name: Install nginx and nodejs 12 Packages\n"
+        "  apt:\n"
+        "    name:\n"
+        "    - nginx\n"
+        "    - nodejs\n"
+        "    state: latest\n"
+    ]
+    y = yaml.safe_load
+    assert y(anonymize(sample)[0]) == y(sample[0])
+
+
+def test_anonymize_with_change():
+    sample = ["- name: foo@bar.com\n  password: 'admin'\n  ipv4: '10.13.1.3 '\n"]
+    result = anonymize(sample)
+    new_values = yaml.safe_load(result[0])[0]
+    assert "foo@bar.com" not in new_values["name"]
+    assert new_values["password"] == "admin"
+    assert new_values["ipv4"].startswith("10.13")
+    assert new_values["ipv4"] != "10.13.1.3"
