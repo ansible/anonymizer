8db0e8ec299b301a2c37689f44e24730f3dd3696
Gonéri Le Bouder <goneri@lebouder.net>
2023-05-23 12:43:12 -0400

    tests: don't redefine dedent
    Do not reimport the `dedent()` function.

--- tests/test_anonymizer.py
+++ tests/test_anonymizer.py
@@ -568,8 +568,6 @@ def test_hide_secrets_field_only():
 
 
 def test_hide_secrets_vars_file():
-    from textwrap import dedent
-
     origin = """
     ansible_user: root
     ansible_host: esxi1-gw.ws.testing.ansible.com
@@ -584,8 +582,6 @@ def test_hide_secrets_vars_file():
 
 
 def test_hide_secrets_unquoted_string():
-    from textwrap import dedent
-
     origin = """
     ansible_password: an unquoted string
     """
